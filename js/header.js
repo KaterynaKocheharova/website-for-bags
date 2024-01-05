8a9e89d012a055b776655bdf93ea8413da25ca4e
KaterynaKocheharova <kot41901@gmail.com>
2024-01-05 14:24:53 +0200

    menu done

--- js/header.js
+++ js/header.js
@@ -2,7 +2,9 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const myBurgerButton = document.getElementById("myBurgerButton");
+  const myMobileMenu = document.getElementById("mobileMenu");
   myBurgerButton.addEventListener("click", function () {
     myBurgerButton.classList.toggle("clicked");
+    myMobileMenu.classList.toggle("is-open");
   });
 });
